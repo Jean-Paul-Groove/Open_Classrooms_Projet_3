b99cdbf4699d8fb49a2e06027cd8914c26ce1d7a
Quentin <rousseltin@gmail.com>
2023-01-18 20:08:22 +0100

    feature: ajout des filtres dans la galerie de projets

--- FrontEnd/js/work.js
+++ FrontEnd/js/work.js
@@ -8,8 +8,27 @@ class Work {
         this.category = workJSON.category
     }
 }
+function filterWorks (categoryId) {
+    button = document.querySelector(`.button-${categoryId}`)
+    const filterButtons = document.querySelectorAll(".filter_button")
+    filterButtons.forEach( filter => {
+        filter.classList.remove("active_button")
+    })
+    button.classList.add("active_button")
+    if (categoryId === 0){
+        displayWorks(totalList)
+    }else{
+        let filteredList = [];
+        for (let work of totalList){
+            if (work.categoryId === categoryId){
+                filteredList.push(work)
+            }
+            displayWorks(filteredList)
+        }}    
+}
 //Fonction pour afficher la liste de travaux
-function displayWorks (list){    
+function displayWorks (list){
+    gallery.innerHTML=""    
     list.forEach( work => {
         let figure = document.createElement("figure")
         figure.innerHTML = `<img crossorigin="anonymous" src="${work.imageUrl}" alt="${work.title}">`
@@ -18,6 +37,25 @@ function displayWorks (list){
         figure.appendChild(figcaption)
         gallery.appendChild(figure)}
         )
+}
+//Fonction pour afficher les filtres en fonctions des catégories
+function createFilters(categoriesList){
+    let filtersContainer = document.createElement("div")
+          filtersContainer.classList.add("filtersContainer")
+          portfolio.insertBefore(filtersContainer, gallery)
+          let buttonAll = document.createElement("button")
+          buttonAll.innerHTML = "Tous"
+          buttonAll.classList.add("filter_button", "button-0")
+          buttonAll.addEventListener('click', () =>{filterWorks(0)})
+          filtersContainer.appendChild(buttonAll)
+          for (let category of categoriesList){
+            let button = document.createElement("button")
+            console.log(category[1].name)
+            button.innerHTML = category[1].name
+            button.classList.add(`filter_button`, `button-${category[1].id}`)
+            filtersContainer.appendChild(button)
+            button.addEventListener('click', () =>{filterWorks(category[1].id)})
+          }
 }
 // fonction pour actualiser la liste de travaux
 function fetchListOfWorks(){
@@ -28,18 +66,29 @@ function fetchListOfWorks(){
           for (let workJSON of listJSON) {
               let work = new Work (workJSON)
               totalList.push(work)
+              categoriesList.set(work.categoryId, work.category)
               }
-          displayWorks(totalList)     
+          displayWorks(totalList)
+          console.log(categoriesList)
+          createFilters(categoriesList)
+          
     })
 }
+
 
  
 //On récupère le conteneur ".gallery"
 const gallery = document.querySelector(".gallery")
+const portfolio = document.getElementById("portfolio")
 let totalList = []
-        
+//Map des catégories existantes de filtres
+const categoriesList = new Map();
 //On fait le premier affichage
 fetchListOfWorks()
+console.log(totalList)
+
+//filtres : 
+
 
     
    
